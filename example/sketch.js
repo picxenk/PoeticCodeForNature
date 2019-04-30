9b8c33582b879c6f1b4f1d0437d20b806f408707
Seungbum Kim <picxenk@gmail.com>
2019-04-30 20:55:38 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -1,5 +1,5 @@
 function setup() {
-  title = createElement('h2', "작품 제목");
+  title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
   title.position(20, 0);
 
   canvas = createCanvas(300, 300);
@@ -7,13 +7,13 @@ function setup() {
   canvas.class("artwork");
 
   description = "\
-  작품에 대한 설명이 들어갑니다. <br/>
-  HTML이 직접 들어가서 줄넘김을 할 수 있습니다.
+  작품에 대한 설명이 들어갑니다. <br/> \
+  HTML이 직접 들어가서 줄넘김을 할 수 있습니다. \
   ";
   text = createDiv(description);
-  text.position(350, 60);
+  text.position(20, 400);
   text.style("font-family", "monospace");
-  text.style("font-size", "16pt");
+  text.style("font-size", "12pt");
 
 }
 
