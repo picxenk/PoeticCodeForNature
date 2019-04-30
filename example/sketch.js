23d216043a3550182b366b8408cd476a6bd52734
Seungbum Kim <picxenk@gmail.com>
2019-04-30 20:44:55 +0900

    Update sketch.js

--- example/sketch.js
+++ example/sketch.js
@@ -6,6 +6,15 @@ function setup() {
   canvas.position(20, 60);
   canvas.class("artwork");
 
+  description = "
+  작품에 대한 설명이 들어갑니다. <br/>
+  HTML이 직접 들어가서 줄넘김을 할 수 있습니다.
+  ";
+  text = createDiv(description);
+  text.position(350, 60);
+  text.style("font-family", "monospace");
+  text.style("font-size", "16pt");
+
 }
 
 function draw() {
