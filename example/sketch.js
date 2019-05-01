ea8b04cf0632702fc8246da38457701b90468146
Seungbum Kim <picxenk@gmail.com>
2019-05-01 11:50:47 +0900

    fix bug

--- example/sketch.js
+++ example/sketch.js
@@ -2,7 +2,7 @@ function setup() {
   title = createElement('h2', "<a href='/PoeticCodeForNature'> HOME : </a> 작품 제목");
   title.position(20, 0);
 
-  canvas = createCanvas(400, 400;
+  canvas = createCanvas(400, 400);
   canvas.position(20, 60);
   canvas.class("artwork");
 
